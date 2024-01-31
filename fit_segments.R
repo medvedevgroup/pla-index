46e0877ca499f7f60cff4785cc2d50eb74a19773
Md Hasin Abrar <hasinabrar1405@gmail.com>
2024-01-31 14:34:54 -0500

    code to fit segments and csv for pla-complexity
    fit_segments.R
    R code to generate fit segments and get the alpha and beta value. File name has to be modified into
    the code to fit the segments against epsilon.
    
    RefSeq_dataset.csv
    csv file containing the genome, kingdom, alpha and beta value for all 549 genomes
    
    RefseqID Table 6.pdf
    RefSeq IDs of the seven representative genomes in table 6 from our paper.

---
+++ fit_segments.R
@@ -0,0 +1,41 @@
+model <- function(x, c, a) {
+  c * x^(-a)
+}
+
+fit_segments <- function(segments_fn, genome_len){
+  lines <- readLines(segments_fn)
+  
+  data <- read.table(text=lines[1:length(lines)], header = FALSE, col.names = c("x", "y"))
+  
+  x <- data$x
+  y <- data$y
+  
+  # Fit the model to the data
+  fit <- nls(y ~ model(x, c, a), start = list(c = data$y[length(data$y)], a = 1))
+  
+  param_coef <- coef(fit)
+  c <- param_coef["c"]
+  a <- param_coef["a"]
+  
+  c_prime <- c / genome_len
+  
+  y_pred = predict(fit)
+  
+  rmse = sqrt(mean((y - y_pred)^2))
+  
+  summary_result <- summary(fit)
+  
+  p_values <- summary_result$coefficients[, "Pr(>|t|)"]
+  # print(paste(p_values[1], p_values[2]))
+  
+  # Extract significant values based on a significance level (e.g., 0.05)
+  significant_values <- summary_result$coefficients[p_values < 0.05, ]
+  
+  print(significant_values)
+  
+  print(paste("Fitted degree (alpha):", a,"Fitted constatnt (Beta): ",c_prime))
+}
+
+segments_fn <- "Gorilla.segments.txt"
+genome_len <- 3595314213
+fit_segments(segments_fn, genome_len)
